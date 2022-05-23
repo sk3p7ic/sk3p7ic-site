05f52f109eb8d59b8ef5618935b444d42213f27f
Joshua Ibrom <joshuaibrom@gmail.com>
2022-05-23 12:13:56 -0500

    Added slight responsiveness to home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,8 +3,8 @@ import { AboutPanel } from "./about-panel/AboutPanel";
 
 export const HomePage = () => {
   return (
-    <div className="flex-grow w-full h-full p-32 flex gap-4">
-      <div className="flex flex-col gap-8 justify-center">
+    <div className="flex-grow w-full h-full p-32 flex flex-col md:flex-row md:gap-4">
+      <div className="flex flex-col gap-8 justify-center w-min">
         <h1 className="font-telex text-6xl whitespace-nowrap">
           Programmer / Web Dev.
           <br />
@@ -21,8 +21,8 @@ export const HomePage = () => {
           </FunkyButton>
         </div>
       </div>
-      <div className="flex-grow flex justify-center">
-        <div className="w-3/4 p-8 border-8 rounded-3xl border-stone-900 bg-amber-50 ">
+      <div className="flex-grow flex 2xl:justify-center">
+        <div className="flex-grow w-full xl:flex-grow-0 2xl:w-3/4 p-8 border-8 rounded-3xl border-stone-900 bg-amber-50 ">
           <div className="flex flex-col font-telex text-xl">
             <p className="text-justify">
               A computer science student and coffee lover from San Antonio,
