b6e349de862f74e6f97abb9f70472ba9aa5f9528
Joshua Ibrom <joshuaibrom@gmail.com>
2022-05-23 07:49:08 -0500

    Changed element to anchor tag

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -8,21 +8,16 @@ export const FunkyButton = (props) => {
   const buttonColor =
     props.buttonColor != null ? props.buttonColor : "bg-purple-900";
 
-  const doHandleClick = (e) => {
-    e.preventDefault();
-    window.location.replace(props.href);
-  };
-
   return (
-    <button
+    <a
       className={
         "p-3 font-tomorrow text-4xl text-stone-100 drop-shadow-[6px_6px_0px_rgba(0,0,0,1)] rounded-sm -rotate-2 transition-all" +
         " hover:drop-shadow-[10px_10px_0px_rgba(0,0,0,1)] hover:rounded-lg hover:rotate-0 hover:scale-110 " +
         buttonColor
       }
-      onClick={doHandleClick}
+      href={props.href}
     >
       {props.children}
-    </button>
+    </a>
   );
 };
