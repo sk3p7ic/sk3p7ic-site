f86f5dad35e77ca1b4047f482d599fa282a2f6b7
Joshua Ibrom <joshuaibrom@gmail.com>
2022-05-23 12:36:33 -0500

    Buttons are now responsive

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -11,7 +11,7 @@ export const FunkyButton = (props) => {
   return (
     <a
       className={
-        "p-3 font-tomorrow text-4xl text-stone-100 drop-shadow-[6px_6px_0px_rgba(0,0,0,1)] rounded-sm -rotate-2 transition-all" +
+        "p-3 font-tomorrow md:text-4xl text-stone-100 drop-shadow-[6px_6px_0px_rgba(0,0,0,1)] rounded-sm -rotate-2 transition-all" +
         " hover:drop-shadow-[10px_10px_0px_rgba(0,0,0,1)] hover:rounded-lg hover:rotate-0 hover:scale-110 " +
         buttonColor
       }
