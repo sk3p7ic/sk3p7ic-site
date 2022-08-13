5250e43ae89bb504c0fd214563aab428ad2c9262
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-12 23:52:52 -0500

    Started working on the Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
+import MainNavbar from "./components/MainNavbar.vue";
 </script>
 
 <template>
+  <MainNavbar />
   <RouterView />
 </template>
 
---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="w-screen px-72 py-2 font-inter">
+    <div class="flex flex-row justify-between items-center">
+      <h1 class="text-2xl">sk3p7ic</h1>
+      <ul class="flex flex-row gap-4">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style></style>
