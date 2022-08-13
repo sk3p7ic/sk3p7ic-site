2f9f13826738b32d0ecf475d3e04d545b8a975ee
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-13 14:36:06 -0500

    Worked on making Navbar responsive

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,22 +1,57 @@
 <template>
-  <nav class="w-screen px-72 py-2 font-inter">
+  <nav
+    class="w-screen px-4 lg:px-24 xl:px-72 py-2 font-inter bg-gray-800 text-gray-100"
+  >
     <div class="flex flex-row justify-between items-center">
-      <h1 class="text-3xl font-extrabold">sk3p7ic</h1>
-      <ul class="flex flex-row gap-4">
-        <li v-for="(page, index) of pages" :key="index">
-          <a
-            :href="page.href"
+      <h1 class="text-3xl font-extrabold text-fuchsia-400">sk3p7ic</h1>
+      <ul class="hidden md:flex flex-row gap-4">
+        <template v-for="(page, index) of pages" :key="index">
+          <li
             :class="{ active: page.name === $route.name }"
-            class="text-lg"
-            >{{ page.title }}</a
+            class="px-3 py-1 rounded-lg transition-colors"
           >
-        </li>
+            <a
+              :href="page.href"
+              :class="{ 'hover:text-fuchsia-400': page.name !== $route.name }"
+              class="text-lg transition-colors"
+              >{{ page.title }}</a
+            >
+          </li>
+        </template>
       </ul>
+      <Popover class="block md:hidden">
+        <PopoverButton
+          class="p-1 rounded-md text-fuchsia-400 border border-fuchsia-300 focus:text-cyan-400"
+        >
+          <MenuIcon class="w-8 h-8" />
+        </PopoverButton>
+        <PopoverPanel
+          class="absolute left-0 top-14 w-screen z-10 p-4 bg-gray-900"
+        >
+          <template v-for="(page, index) of pages" :key="index">
+            <a
+              :class="{ active: page.name === $route.name }"
+              class="block my-2 px-3 py-1 rounded-lg transition-colors"
+            >
+              <span
+                :href="page.href"
+                :class="{
+                  'hover:text-fuchsia-400': page.name !== $route.name,
+                }"
+                class="text-lg transition-colors"
+                >{{ page.title }}</span
+              >
+            </a>
+          </template>
+        </PopoverPanel>
+      </Popover>
     </div>
   </nav>
 </template>
 
 <script setup>
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { MenuIcon } from "@heroicons/vue/outline";
 import { ref } from "@vue/reactivity";
 
 const pages = ref([
@@ -29,6 +64,6 @@ const pages = ref([
 
 <style scoped>
 .active {
-  @apply underline;
+  @apply bg-cyan-400 text-gray-800 hover:bg-cyan-500;
 }
 </style>
