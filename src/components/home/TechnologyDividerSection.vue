3ef4cbdbdefdbcf713f6e92d41f90aec12e8ff4f
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-13 17:18:33 -0500

    Worked on section for showing technologies I know

--- src/components/home/TechnologyDividerSection.vue
+++ src/components/home/TechnologyDividerSection.vue
@@ -1,7 +1,42 @@
 <template>
-  <div class="w-full bg-gray-900 text-gray-100 py-8">Lorem ipsum.</div>
+  <div class="w-full bg-gray-900 text-gray-100 py-8">
+    <h2 class="px-2 pb-8 font-inter font-bold text-3xl text-center">
+      Experienced in the industry standards.
+    </h2>
+    <div
+      class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 align-center"
+    >
+      <div
+        v-for="(tech, index) of techs"
+        :key="index"
+        class="flex flex-col items-center gap-2"
+      >
+        <component :is="tech.icon" class="fill-gray-100 w-14 h-14" />
+        <p>{{ tech.name }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "@vue/reactivity";
+import {
+  AngularIcon,
+  FlutterIcon,
+  MongodbIcon,
+  NodedotjsIcon,
+  ReactIcon,
+  VuedotjsIcon,
+} from "vue3-simple-icons";
+
+const techs = ref([
+  { icon: ReactIcon, name: "React" },
+  { icon: AngularIcon, name: "Angular" },
+  { icon: VuedotjsIcon, name: "Vue" },
+  { icon: NodedotjsIcon, name: "Node.js" },
+  { icon: MongodbIcon, name: "MongoDB" },
+  { icon: FlutterIcon, name: "Flutter" },
+]);
+</script>
 
 <style></style>
