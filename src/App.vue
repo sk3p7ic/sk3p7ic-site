1439fd34e920d843954e6ef501167dec438d26ec
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-13 14:57:19 -0500

    Changed default styling for pages

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import MainNavbar from "./components/MainNavbar.vue";
 <template>
   <div class="w-screen h-screen flex flex-col">
     <MainNavbar />
-    <RouterView class="flex-grow-1 max-h-screen overflow-auto" />
+    <RouterView
+      class="flex-grow-1 max-h-screen overflow-auto bg-gray-100 text-gray-800"
+    />
   </div>
 </template>
 
