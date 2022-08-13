ded58dfcc0c131920f7fd9896f1bdc74b0d03ec4
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-13 15:27:25 -0500

    Started working on divider to show techs

---
+++ src/components/home/TechnologyDividerSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full bg-gray-900 text-gray-100 py-8">Lorem ipsum.</div>
+</template>
+
+<script setup></script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="flex-grow-1 max-h-screen overflow-auto bg-gray-100 text-gray-800">
     <HomeHero />
-    <p>Some text</p>
+    <TechnologyDividerSection />
   </div>
 </template>
 
 <script setup>
 import HomeHero from "../components/home/HomeHero.vue";
+import TechnologyDividerSection from "../components/home/TechnologyDividerSection.vue";
 </script>
 
 <style></style>
