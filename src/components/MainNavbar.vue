027d1bc47b67add064384aa9545c0d1117fc4534
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-14 12:30:29 -0500

    Added transition effect for mobile nav menu

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -25,25 +25,34 @@
         >
           <MenuIcon class="w-8 h-8" />
         </PopoverButton>
-        <PopoverPanel
-          class="absolute left-0 top-14 w-screen z-10 p-4 bg-gray-900"
+        <transition
+          enter-active-class="transition ease-out duration-200"
+          enter-from-class="opacity-0 translate-y-1"
+          enter-to-class="opacity-100 translate-y-0"
+          leave-active-class="transition ease-in duration-150"
+          leave-from-class="opacity-100 translate-y-0"
+          leave-to-class="opacity-0 translate-y-1"
         >
-          <template v-for="(page, index) of pages" :key="index">
-            <a
-              :class="{ active: page.name === $route.name }"
-              class="block my-2 px-3 py-1 rounded-lg transition-colors"
-            >
-              <span
-                :href="page.href"
-                :class="{
-                  'hover:text-fuchsia-400': page.name !== $route.name,
-                }"
-                class="text-lg transition-colors"
-                >{{ page.title }}</span
+          <PopoverPanel
+            class="absolute left-0 top-14 w-screen z-10 p-4 bg-gray-900"
+          >
+            <template v-for="(page, index) of pages" :key="index">
+              <a
+                :class="{ active: page.name === $route.name }"
+                class="block my-2 px-3 py-1 rounded-lg transition-colors"
               >
-            </a>
-          </template>
-        </PopoverPanel>
+                <span
+                  :href="page.href"
+                  :class="{
+                    'hover:text-fuchsia-400': page.name !== $route.name,
+                  }"
+                  class="text-lg transition-colors"
+                  >{{ page.title }}</span
+                >
+              </a>
+            </template>
+          </PopoverPanel></transition
+        >
       </Popover>
     </div>
   </nav>
