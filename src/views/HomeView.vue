3bb420fcd5ff3eda3b4a8cd60d67192b7e2e9a84
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-14 21:00:09 -0500

    Added contact button to bottom of page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,17 @@
     <HomeHero centered />
     <TechnologyDividerSection />
     <Services />
+
+    <div
+      class="flex flex-col gap-6 px-4 py-8 border-t-2 border-t-gray-800 text-center shadow-inner"
+    >
+      <h1 class="font-inter font-extrabold text-5xl">Like what you see?</h1>
+      <a
+        href="/contact"
+        class="mx-auto py-4 px-8 font-inter font-bold text-lg bg-cyan-300 hover:bg-cyan-400 rounded-xl transition-all cursor-pointer"
+        >Reach Out and Contact Me</a
+      >
+    </div>
   </div>
 </template>
 
