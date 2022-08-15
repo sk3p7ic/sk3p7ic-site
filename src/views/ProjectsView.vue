80b18d32f71f2c71d102a354b1eb9b1399e7fd43
Joshua Ibrom <joshuaibrom@gmail.com>
2022-08-14 21:45:15 -0500

    Added projects page

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-gray-100 text-gray-800"></div>
+  <site-footer />
+</template>
+
+<script setup>
+import SiteFooter from "../components/SiteFooter.vue";
+</script>
+
+<style></style>
