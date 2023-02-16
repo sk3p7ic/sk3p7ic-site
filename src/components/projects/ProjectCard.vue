ae1b8bc33a8d3ee3073c2774cac151e7461cde29
Joshua Ibrom <joshuaibrom@gmail.com>
2023-02-16 16:53:11 -0600

    Added back links for GitHub repos.

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -7,25 +7,20 @@
         class="rounded-t-md"
       />
       <div class="absolute bottom-2 right-2 flex flex-row gap-2">
-        <!--
-        <template
-          v-for="({ url, tooltip }, index) in getGithubLinks()"
-          :key="index"
-        >
-          <a :href="url" class="relative bg-gray-900 p-1 rounded-full group">
+        <template v-for="({ title, href }, index) in info.repos" :key="index">
+          <a :href="href" class="relative bg-gray-900 p-1 rounded-full group">
             <GithubIcon class="fill-cyan-500" />
             <div
               class="absolute left-1/2 -translate-x-1/2 bottom-3 items-center hidden mb-6 group-hover:flex flex-col"
             >
               <span
                 class="relative z-10 p-2 text-xs leading-none text-gray-100 whitespace-no-wrap bg-gray-900 shadow-lg"
-                >{{ tooltip }}</span
+                >{{ title }}</span
               >
               <div class="w-3 h-3 -mt-2 rotate-45 bg-gray-900"></div>
             </div>
           </a>
         </template>
--->
       </div>
     </div>
     <div class="flex-grow flex flex-col px-4 py-2 gap-2">
